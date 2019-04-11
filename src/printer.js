d35d8579e9b8cdc43a93472ca9db5b8d5d66b79f
Nathan Morgan <nathan@fuelingtheweb.com>
2019-04-11 12:01:03 -0400

    Increase chained calls on one line

--- src/printer.js
+++ src/printer.js
@@ -548,7 +548,7 @@ function printMemberChain(path, options, print) {
   // If the last group is a function it's okay to inline if it fits.
   if (
     hasComment ||
-    callExpressionCount >= 5 ||
+    callExpressionCount >= 6 ||
     printedGroups.slice(0, -1).some(willBreak)
   ) {
     return group(expanded);
